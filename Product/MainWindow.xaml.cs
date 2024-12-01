1ce380c6ae7a2fdad8e0aabf0e44edd9555d9bea
Yuarqm <yuminov.artem@gmail.com>
2024-12-01 20:34:46 +0500

    Добавьте файлы проекта.

---
+++ Product/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Product
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public List<Product> Products { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(NameBox.Text) || string.IsNullOrEmpty(PriceBox.Text) || string.IsNullOrEmpty(QuanBox.Text))
+            {
+                MessageBox.Show("Пожалуйста, заполните все поля.");
+                return;
+            }
+            if (string.IsNullOrEmpty(QuanBox.Text))
+            {
+                Products.Add(new Product(NameBox.Text, Convert.ToDouble(PriceBox.Text)));
+            }
+            else
+            {
+                Products.Add(new Product(NameBox.Text, Convert.ToDouble(PriceBox.Text), Convert.ToInt32(QuanBox.Text)));
+            }
+
+        }
+    }
+}
---
+++ Product/Product.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace Product
+{
+    public class Product
+    {
+        string Name { get; set; }
+        double Price
+        {
+            get { return Price; }
+            set
+            {
+                if (value < 0)
+                {
+                    MessageBox.Show("Цена не может быть меньше 0");
+                }
+                else
+                {
+                    Price = value;
+                }
+            }
+        }
+        int Quantity
+        {
+            get { return Quantity; }
+            set
+            {
+                if (value < 0)
+                {
+                    MessageBox.Show("Кол-во не может быть меньше 0");
+                }
+                else
+                {
+                    Quantity = value;
+                }
+            }
+        }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+        public Product(string name, double price) : this(name, price, 1)
+        {
+            Name = name;
+            Price = price;
+
+        }
+        public void Deconstruct(out string name, out double price, out int quantity)
+        {
+            name = Name;
+            price = Price;
+            quantity = Quantity;
+        }
+    }
+
+}
